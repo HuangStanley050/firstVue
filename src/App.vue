4f20f56edb6fae9814dcbfb3874b3f6dd261c02d
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-25 09:59:28 +1000

    Finished more component exercise

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
-  <div id="app">
-    <Home/>
+  <div class="container" id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Header/>
+    <hr>
+    <ServerStatus/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-import Home from "./components/Home.vue";
+//import Home from "./components/Home.vue";
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+import ServerStatus from "./components/ServerStatus.vue";
 export default {
   name: "app",
   components: {
-    Home
+    Footer,
+    Header,
+    ServerStatus
   }
 };
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
