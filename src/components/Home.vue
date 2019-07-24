42bf0269d1fdb05ed7fccd47fcdda155e1615294
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-07-25 09:15:16 +1000

    created first component

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Server Status: {{status}}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = "Normal";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
